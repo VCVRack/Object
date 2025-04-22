c1f76a75b1cb8b014c5640a8b8aaae210654301c
Andrew Belt <andrewpbelt@gmail.com>
2025-04-22 04:30:25 -0400

    Add ArrayAccessor class.

--- examples/CppWrapper.h
+++ examples/CppWrapper.h
@@ -24,11 +24,15 @@ const T* CppWrapper_getClass(const Object* self) {
 }
 
 
+/** Base class that wraps an Object and its methods.
+*/
 struct ObjectWrapper {
 	Object* self;
+
 	ObjectWrapper(Object* self) : self(self) {
 		CppWrapper_specialize(self, this);
 	}
+
 	virtual ~ObjectWrapper() {
 		if (self) {
 			Object* oldSelf = self;
@@ -40,18 +44,17 @@ struct ObjectWrapper {
 };
 
 
+/** Behaves like a const variable but wraps a getter function.
+Example:
+	Getter<int, value_get> value(object);
+	int v = value; // Calls value_get(object);
+*/
 template <typename T,
-	T (*Get)(const Object*),
-	void (*Set)(Object*, T)>
-struct Property {
+	T (*Get)(const Object* self)>
+struct Getter {
 	Object* self;
 
-	explicit Property(Object* self) : self(self) {}
-
-	Property& operator=(const T& value) {
-		Set(self, value);
-		return *this;
-	}
+	explicit Getter(Object* self) : self(self) {}
 
 	operator T() const {
 		return Get(self);
@@ -59,7 +62,86 @@ struct Property {
 };
 
 
-// TODO: Getter and Setter classes
+/** Behaves like a mutable variable but wraps getter/setter functions.
+Example:
+	Accessor<int, value_get, value_set> value(object);
+	int n = value; // Calls value_get(object);
+	value = 42; // Calls value_set(object, 42);
+*/
+template <typename T,
+	T (*Get)(const Object* self),
+	void (*Set)(Object* self, T value)>
+struct Accessor : Getter<T, Get> {
+	explicit Accessor(Object* self) : Getter<T, Get>(self) {}
+
+	Accessor& operator=(const T& value) {
+		Set(this->self, value);
+		return *this;
+	}
+};
+
+
+/** Behaves like a const std::array but wraps an element getter function.
+Example:
+	ArrayGetter<int, array_size, array_get> array(object);
+	array.size(); // Calls array_size(object);
+	int n = array[42]; // Calls array_get(object, 42);
+*/
+template <class T,
+	size_t (*Size)(const Object* self),
+	T (*Get)(const Object* self, size_t index)>
+struct ArrayGetter {
+	using value_type = T;
+	using size_type = size_t;
+
+	Object* self;
+
+	explicit ArrayGetter(Object* self) : self(self) {}
+
+	T operator[](size_t index) const {
+		return Get(self, index);
+	}
+
+	size_t size() const {
+		return Size(self);
+	}
+};
+
+
+/** Behaves like a mutable std::array but wraps element getter/setter functions.
+Example:
+	ArrayAccessor<int, array_size, array_get, array_set> array(object);
+	array.size(); // Calls array_size(object);
+	int n = array[42]; // Calls array_get(object, 42);
+	array[42] = 1; // Calls array_set(object, 42, 1);
+*/
+template <class T,
+	size_t (*Size)(const Object* self),
+	T (*Get)(const Object* self, size_t index),
+	void (*Set)(Object* self, size_t index, T value)>
+struct ArrayAccessor : ArrayGetter<T, Size, Get> {
+	explicit ArrayAccessor(Object* self) : ArrayGetter<T, Size, Get>(self) {}
+
+	struct ElementAccessor {
+		Object* self;
+		size_t index;
+
+		ElementAccessor(Object* self, size_t index) : self(self), index(index) {}
+
+		operator T() const {
+			return Get(self, index);
+		}
+
+		ElementAccessor& operator=(const T& value) {
+			Set(self, index, value);
+			return *this;
+		}
+	};
+
+	ElementAccessor operator[](size_t index) {
+		return ElementAccessor(this->self, index);
+	}
+};
 
 
 #endif // __cplusplus
