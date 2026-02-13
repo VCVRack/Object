40b5a32c494025c664851b28c4db63410e224d91
Andrew Belt <andrewpbelt@gmail.com>
2026-02-13 15:06:20 -0500

    Add ObjectRef and WeakObjectRef C++ RAII Object wrappers.

---
+++ Object/ObjectRef.hpp
@@ -0,0 +1,224 @@
+#pragma once
+
+#include "Object.h"
+
+
+/** Holds a strong reference to an Object using C++ RAII.
+T can be `Object` or `const Object`.
+TODO: Move to Object library.
+*/
+template<typename T = Object>
+struct ObjectRefT {
+	T* object = NULL;
+
+	ObjectRefT() = default;
+
+	/** Transfers ownership of an Object from the caller.
+	Does not obtain a new reference.
+	*/
+	ObjectRefT(T* obj) : object(obj) {}
+
+	/** Obtains a new reference of the Object from another ObjectRefT.
+	Both ObjectRefTs then own their own reference.
+	*/
+	ObjectRefT(const ObjectRefT& other) : object(other.object) {
+		Object_obtain(object);
+	}
+
+	/** Obtains a new reference of the Object from another compatible ObjectRefT.
+	Allows implicit conversion from ObjectRef to ConstObjectRef.
+	*/
+	template<typename U>
+	ObjectRefT(const ObjectRefT<U>& other) : object(other.object) {
+		Object_obtain(object);
+	}
+
+	/** Moves a reference from another ObjectRefT. */
+	ObjectRefT(ObjectRefT&& other) : object(other.object) {
+		other.object = NULL;
+	}
+
+	/** Moves a reference from another compatible ObjectRefT. */
+	template<typename U>
+	ObjectRefT(ObjectRefT<U>&& other) : object(other.object) {
+		other.object = NULL;
+	}
+
+	~ObjectRefT() {
+		Object_release(object);
+	}
+
+	ObjectRefT& operator=(T* obj) {
+		Object_release(object);
+		object = obj;
+		return *this;
+	}
+
+	ObjectRefT& operator=(const ObjectRefT& other) {
+		Object_obtain(other.object);
+		Object_release(object);
+		object = other.object;
+		return *this;
+	}
+
+	template<typename U>
+	ObjectRefT& operator=(const ObjectRefT<U>& other) {
+		Object_obtain(other.object);
+		Object_release(object);
+		object = other.object;
+		return *this;
+	}
+
+	ObjectRefT& operator=(ObjectRefT&& other) {
+		if (this != &other) {
+			Object_release(object);
+			object = other.object;
+			other.object = NULL;
+		}
+		return *this;
+	}
+
+	template<typename U>
+	ObjectRefT& operator=(ObjectRefT<U>&& other) {
+		Object_release(object);
+		object = other.object;
+		other.object = NULL;
+		return *this;
+	}
+
+	/** Releases ownership of the Object and transfers ownership to the caller.
+	*/
+	T* release() {
+		T* obj = object;
+		object = NULL;
+		return obj;
+	}
+
+	/** Obtains a new reference of the Object.
+	*/
+	static ObjectRefT obtain(T* obj) {
+		Object_obtain(obj);
+		ObjectRefT ref;
+		ref.object = obj;
+		return ref;
+	}
+
+	operator T*() const { return object; }
+};
+
+using ObjectRef = ObjectRefT<Object>;
+using ConstObjectRef = ObjectRefT<const Object>;
+
+
+/** Holds a weak reference to an Object using C++ RAII.
+A weak reference does not prevent the object from being freed, but can be converted to a strong ObjectRef if still alive with lock().
+T can be `Object` or `const Object`.
+TODO: Move to Object library.
+*/
+template<typename T = Object>
+struct WeakObjectRefT {
+	T* object = NULL;
+
+	WeakObjectRefT() = default;
+
+	/** Obtains a weak reference of an Object. */
+	WeakObjectRefT(T* obj) : object(obj) {
+		Object_weak_obtain(object);
+	}
+
+	/** Obtains a new weak reference of the Object from an ObjectRef. */
+	template<typename U>
+	WeakObjectRefT(const ObjectRefT<U>& other) : object(other.object) {
+		Object_weak_obtain(object);
+	}
+
+	/** Obtains a new weak reference of the Object from another WeakObjectRefT. */
+	WeakObjectRefT(const WeakObjectRefT& other) : object(other.object) {
+		Object_weak_obtain(object);
+	}
+
+	/** Obtains a new weak reference of the Object from another compatible WeakObjectRefT.
+	Allows implicit conversion from WeakObjectRef to ConstWeakObjectRef.
+	*/
+	template<typename U>
+	WeakObjectRefT(const WeakObjectRefT<U>& other) : object(other.object) {
+		Object_weak_obtain(object);
+	}
+
+	/** Moves a weak reference from another WeakObjectRefT. */
+	WeakObjectRefT(WeakObjectRefT&& other) : object(other.object) {
+		other.object = NULL;
+	}
+
+	/** Moves a weak reference from another compatible WeakObjectRefT. */
+	template<typename U>
+	WeakObjectRefT(WeakObjectRefT<U>&& other) : object(other.object) {
+		other.object = NULL;
+	}
+
+	~WeakObjectRefT() {
+		Object_weak_release(object);
+	}
+
+	WeakObjectRefT& operator=(T* obj) {
+		Object_weak_obtain(obj);
+		Object_weak_release(object);
+		object = obj;
+		return *this;
+	}
+
+	template<typename U>
+	WeakObjectRefT& operator=(const ObjectRefT<U>& other) {
+		T* obj = other;
+		Object_weak_obtain(obj);
+		Object_weak_release(object);
+		object = obj;
+		return *this;
+	}
+
+	WeakObjectRefT& operator=(const WeakObjectRefT& other) {
+		Object_weak_obtain(other.object);
+		Object_weak_release(object);
+		object = other.object;
+		return *this;
+	}
+
+	template<typename U>
+	WeakObjectRefT& operator=(const WeakObjectRefT<U>& other) {
+		Object_weak_obtain(other.object);
+		Object_weak_release(object);
+		object = other.object;
+		return *this;
+	}
+
+	WeakObjectRefT& operator=(WeakObjectRefT&& other) {
+		if (this != &other) {
+			Object_weak_release(object);
+			object = other.object;
+			other.object = NULL;
+		}
+		return *this;
+	}
+
+	template<typename U>
+	WeakObjectRefT& operator=(WeakObjectRefT<U>&& other) {
+		Object_weak_release(object);
+		object = other.object;
+		other.object = NULL;
+		return *this;
+	}
+
+	/** Attempts to obtain a strong reference from this weak reference.
+	Returns an ObjectRefT holding the strong reference, or an empty ObjectRefT if the object has been freed.
+	*/
+	ObjectRefT<T> lock() const {
+		if (Object_weak_lock(object))
+			return ObjectRefT<T>(object);
+		return ObjectRefT<T>();
+	}
+
+	operator T*() const { return object; }
+};
+
+using WeakObjectRef = WeakObjectRefT<Object>;
+using ConstWeakObjectRef = WeakObjectRefT<const Object>;
